76ac500d78d54260291e66a88ea1a62248eeafd9
Alex <alexanderrud0603@gmail.com>
2015-03-27 16:58:13 +0300

    modify entity

--- src/com/adform/academy/data/entity/AbstractScheme.java
+++ src/com/adform/academy/data/entity/AbstractScheme.java
@@ -9,4 +9,20 @@ public abstract class AbstractScheme {
         this.name = name;
         this.version = version;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getVersion() {
+        return version;
+    }
+
+    public void setVersion(double version) {
+        this.version = version;
+    }
 }
