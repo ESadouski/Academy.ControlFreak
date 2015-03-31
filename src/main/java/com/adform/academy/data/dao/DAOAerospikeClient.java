94f185147b8d649c3430f545a2f75de4389df3d0
ESadouski <zsadouski@gmail.com>
2015-03-31 18:32:11 +0300

    Create simple addScheme method

--- src/main/java/com/adform/academy/data/dao/DAOAerospikeClient.java
+++ src/main/java/com/adform/academy/data/dao/DAOAerospikeClient.java
@@ -2,6 +2,9 @@ package com.adform.academy.data.dao;
 
 import com.adform.academy.data.entity.Scheme;
 import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Bin;
+import com.aerospike.client.Key;
+import com.aerospike.client.policy.WritePolicy;
 
 import java.util.List;
 
@@ -9,6 +12,9 @@ public class DAOAerospikeClient implements DAOClient {
 
     private static final String HOST = "localhost";
     private static final int PORT = 3000;
+
+    private static final String DBNAME = "first";
+    private static WritePolicy policy = new WritePolicy();
 
     private static DAOAerospikeClient instance = new DAOAerospikeClient();
 
@@ -26,7 +32,12 @@ public class DAOAerospikeClient implements DAOClient {
         String name = scheme.getName();
         double version = scheme.getVersion();
 
+        Key key = new Key(DBNAME, group, name + version);
 
+        Bin nameBin = new Bin("name", name);
+        Bin versBin = new Bin("version", version);
+
+        client.put(policy, key, nameBin, versBin);
     }
 
     @Override
